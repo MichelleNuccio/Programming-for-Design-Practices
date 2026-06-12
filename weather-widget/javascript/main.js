e9dbe4cf7d3bdca6f7ddaae3f557b2aba6865723
MichelleNuccio <nucciomichelle99@gmail.com>
2026-06-12 17:39:52 +0200

    Data and APIs
    Interactive Web submission

---
+++ weather-widget/javascript/main.js
@@ -0,0 +1,11 @@
+console.log("this works");
+
+const API_KEY = config.WEATHER_API_KEY;
+
+function getWeatherData() {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+getWeatherData();
