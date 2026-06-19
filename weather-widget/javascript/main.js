4fbdf48ecbcaf9b76a078aef8253400174af3de9
MichelleNuccio <nucciomichelle99@gmail.com>
2026-06-19 11:16:09 +0200

    DOM Manipulation
    Interactive Web submission

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -3,23 +3,39 @@ console.log("this works");
 const API_KEY = config.WEATHER_API_KEY;
 
 function getWeatherData(zip) {
-  const API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${API_KEY}`;
+  const API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${API_KEY}`;
 
   fetch(API_ENDPOINT)
     .then(response => response.json())
     .then(data =>{
-      // store the data in a variable of our choosing
+      
       let local_weather_data = data;
-      // log the data to the browser console
-      console.log(local_weather_data);
+      let weather_in_celsius = Math.round(
+      local_weather_data.main.temp - 273
+    );   
+    
+    let WEATHER_ICON = local_weather_data.weather[0].icon;
+      CITY_NAME.textContent = local_weather_data.name;
+      CITY_TEMP.textContent = weather_in_celsius + "°C";
+      console.log(WEATHER_ICON);
+      image.setAttribute('src', `https://openweathermap.org/img/wn/${WEATHER_ICON}@2x.png`);
     });
+      
+    form.reset();
+    input.focus();
 }
 
 const searchButton = document.querySelector(".search-button");
-let input = document.querySelector(".zipcode"); 
+let input = document.querySelector(".zipcode");
+let form = document.querySelector("form");
+
+let CITY_NAME = document.querySelector(".city_name");
+let CITY_TEMP = document.querySelector(".temperature"); 
+let image = document.querySelector("img");
 
 function getZipCode(e) {
   e.preventDefault();
+
   let ZIP_CODE = input.value;
   getWeatherData(ZIP_CODE);
 }
